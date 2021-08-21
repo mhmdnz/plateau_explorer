bbe74b15606e19f10c1f4d525cf1e8569beec116
Mohammad Nazari <mohammad.nazari@devolon.fi>
2021-08-21 12:43:11 +0430

    Add unit test for NormalizeRoverLocationFaceService

---
+++ tests/Unit/NormalizeRoverLocationFaceServiceTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\DTOs\LocationFaceDTO;
+use App\Services\NormalizeRoverLocationFaceService;
+use phpDocumentor\Reflection\Location;
+use PHPUnit\Framework\TestCase;
+
+class NormalizeRoverLocationFaceServiceTest extends TestCase
+{
+    public function testNormalizeRoverLocationFaceService()
+    {
+        //ARRANGE
+        $sut = $this->resolveAction();
+
+        // ACT
+        $result = $sut('1 3 e');
+
+        //ASSERT
+        $this->assertInstanceOf(LocationFaceDTO::class, $result);
+        $this->assertEquals(1, $result->locationDTO->x);
+        $this->assertEquals(3, $result->locationDTO->y);
+        $this->assertEquals(LocationFaceDTO::EAST, $result->facing);
+    }
+
+    private function resolveAction(): NormalizeRoverLocationFaceService
+    {
+        return resolve(NormalizeRoverLocationFaceService::class);
+    }
+}
